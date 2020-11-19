776fe9d4646c8a80f0bb147ade248dc9efcee4ea
qwe <qwe>
2020-11-19 15:41:48 +0300

    ***

--- 000_HelloWorld/_000_Start.bat
+++ 000_HelloWorld/_000_Start.bat
@@ -1,8 +1,8 @@
 @echo off
 echo %cd%
-cd C:\Fasm\Projects\000_HelloWorld
+cd C:\D\Git_Hub\HowTo_FASM\000_HelloWorld
 echo %cd%
 set FileName=_000_
-C:\Fasm\FASM.EXE %FileName%.ASM
+C:\D\Git_Hub\HowTo_FASM\Fasm\FASM.EXE %FileName%.ASM
 "C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe" %FileName%.COM
 ::pause
