f1b8cb205d3445fe43c1356e197da1da594ca53e
qwe <qwe>
2020-11-27 11:24:16 +0300

    ***

---
+++ 002_Lib_V2/_003_Start.bat
@@ -0,0 +1,8 @@
+@echo off
+echo %cd%
+cd C:\D\Git_Hub\HowTo_FASM\000_HelloWorld
+echo %cd%
+set FileName=_003_
+C:\D\Git_Hub\HowTo_FASM\Fasm\FASM.EXE %FileName%.ASM
+"C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe" %FileName%.COM
+::pause
