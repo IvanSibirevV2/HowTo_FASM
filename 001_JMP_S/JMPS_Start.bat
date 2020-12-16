e2d2e55c8d9a3388d5bc47bf0a25ffbc2fc75efc
qwe <qwe>
2020-12-16 19:41:43 +0300

    ****

--- 001_JMP_S/JMPS_Start.bat
+++ 001_JMP_S/JMPS_Start.bat
@@ -1,9 +1,9 @@
 @echo off
 echo %cd%
 ::cd C:\Fasm\Projects\001_JMP_S
-cd C:\D\Git_Hub\HowTo_FASM\001_JMP_S
+cd C:\D\Git_Hub\HowTo_FASM\000_HelloWorld
 echo %cd%
-set FileName=jnz
+set FileName=JMPS
 ::C:\Fasm\FASM.EXE %FileName%.ASM
 C:\D\Git_Hub\HowTo_FASM\Fasm\FASM.EXE %FileName%.ASM
 "C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe" %FileName%.COM
