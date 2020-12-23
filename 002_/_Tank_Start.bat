2bed55f2c5daf8834fd5a9dee74926b2de516e10
qwe <qwe>
2020-12-23 16:31:23 +0300

    ***

--- 002_/_Tank_Start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo %cd%
-cd C:\D\Git_Hub\HowTo_FASM\002_
-echo %cd%
-set FileName=_Tank_
-C:\D\Git_Hub\HowTo_FASM\Fasm\FASM.EXE %FileName%.ASM
-"C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe" %FileName%.COM
-::pause
